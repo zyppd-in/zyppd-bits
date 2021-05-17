19c6149116c41b1ae21340fca9aa4a8bff0d5fae
Oliver Meredith <mail@olivermeredith.com>
2021-05-17 07:44:23 +0100

    Updates

---
+++ precache-manifest.d8863c53016bed3c31d008c12c12ddd0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86361f2dcbd1334cb63ddd4c8b7db4da",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a59e5a16db3237e2ed43",
+    "url": "./static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "1c932fb5f201e8de7ded",
+    "url": "./static/js/2.3a05b3ce.chunk.js"
+  },
+  {
+    "revision": "a032f30ebb86a2b7add41ddcb1fee22d",
+    "url": "./static/js/2.3a05b3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a59e5a16db3237e2ed43",
+    "url": "./static/js/main.459bbd77.chunk.js"
+  },
+  {
+    "revision": "59bd054f3bde6b0e219b610549d13f6f",
+    "url": "./static/js/main.459bbd77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a0aa2131abc999a686",
+    "url": "./static/js/runtime-main.c8fcf1a7.js"
+  }
+]);
